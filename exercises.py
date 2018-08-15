d7de95aa73f638e00676cabb757a207ef394143d
gesab001 <hpminigesab001@gmail.com>
2018-08-16 10:09:09 +1200

    completed most exercises

---
+++ exercises.py
@@ -0,0 +1,54 @@
+class Dog():
+    age = 0
+    name = ""
+    weight = 0
+
+rambo = Dog()
+
+class Person():
+    name =""
+    cellPhone = ""
+    email = ""
+
+giovanni = Person()
+abigail = Person()
+giovanni.name = "giovanni"
+giovanni.cellPhone = "0273637989"
+giovanni.email = "144000giovanni@gmail.com"
+
+giovanni.name = "abigail"
+giovanni.cellPhone = "0273637985"
+giovanni.email = "aby318@gmail.com"
+
+class Bird():
+    color = ""
+    name = ""
+    breed = ""
+
+class Mario():
+    position = ""
+    name = ""
+    strength = ""
+
+class Person():
+    name = ""
+    money = 0
+
+nancy = Person()
+nancy.name = "Nancy"
+nancy.money = 100
+
+bob = Person()
+bob.name = "Bob"
+bob.money = 50
+
+print(bob.name, "has", bob.money, "dollars.")
+
+class Animal():
+    name = ""
+
+def eat():
+    return "munch munch"
+
+def makeNoise():
+    return "Grrrs says "
