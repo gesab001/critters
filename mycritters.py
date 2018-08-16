1c6522636e4e74b7c4f56f506cb1780a0bab1649
gesab001 <giovanni@giovanni-lg>
2018-08-16 13:38:08 +1200

    created caterpillars adding size attribute

--- mycritters.py
+++ mycritters.py
@@ -12,16 +12,23 @@ brown     = ( 125, 100, 100)
 
 class Caterpillar:
     def __init__(self):
+        bugSize = random.randrange(0,4)
         x = random.randrange(50,950)
         y = random.randrange(50, 950)
         self.xcoord = x
         self.ycoord = y
-
+        self.size = bugSize
       
     def draw_critter(self, screen):
         x = self.xcoord 
         y = self.ycoord
-        pygame.draw.ellipse(screen,red, [x, y, 40, 45])
+        size = self.size
+        incrementX = 25
+        while size >0:
+           pygame.draw.ellipse(screen, brown, [x+incrementX, y, 40, 45])
+           incrementX+=25
+           size-=1
+        pygame.draw.ellipse(screen, red, [x, y, 40, 45])
         pygame.draw.ellipse(screen,black, [x+6, y+10, 10, 15])
         pygame.draw.ellipse(screen,black, [x+24, y+10, 10, 15])
         pygame.draw.line(screen,black, (x+11, y+1), (x+9, y-10), 3)
